8a6a35db737cbe9da94f08ec9ecff46327f09bba
Your Name <you@example.com>
2026-03-24 16:57:43 +0200

    add list of attakt drons and pupup

--- client/src/componentas/attackPanel.jsx
+++ client/src/componentas/attackPanel.jsx
@@ -1,26 +1,81 @@
+import { useState } from "react";
+
 export default function AttackPanel({ squads, onEngage }) {
+    const [selectedDrone, setSelectedDrone] = useState(null);
     if (!squads) return null;
-  
+
     return (
-      <div style={{ position: "absolute", right: 10, top: 10, background: "white", padding: 10 ,zIndex: 9999}}>
-        <h3>Attack Panel</h3>
-  
-        {squads.map((squad) =>
-          squad.drones.map((drone) => (
-            <div key={drone.drone_id}>
-              <p>id: {drone.drone_id}</p>
-              <p>Ammo: {drone.weapons_ready}</p>
-              <p>Status: {drone.status}</p>
-  
-              <button
-                onClick={() => onEngage(drone.drone_id)}
-                disabled={drone.weapons_ready === 0}
-              >
-                ENGAGE
-              </button>
+        <div style={{ position: "absolute", right: "60px", top: "40px", background: "white", padding: 10, zIndex: 9999, width: "180px", maxWidth: "90vw", boxSizing: "border-box" }}>
+            <h3>Attack Panel</h3>
+            <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+                {squads.map((squad) =>
+                    squad.drones.map((drone) => (
+                        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "5px" }} key={drone.drone_id}>
+                            <p style={{
+                                fontSize: "0.7rem",
+                                margin: 0
+                            }}>id: {drone.drone_id}</p>
+                            {/* <p>Ammo: {drone.weapons_ready}</p>
+              <p>Status: {drone.status}</p> */}
+
+                            <button style={{ width: "3rem", fontSize: "0.5rem", height: "1rem", background: "blue", border: 0, color: "white" }}
+                                onClick={() => {
+                                    setSelectedDrone(drone)
+                                }}
+                                disabled={drone.weapons_ready === 0}
+                            >
+                                ENTER
+                            </button>
+                        </div>
+
+                    ))
+                )}
             </div>
-          ))
-        )}
-      </div>
-    );
-  }
+            {selectedDrone && (
+                <div
+                    style={{
+                        position: "absolute",
+                        top: "120px",
+                        right: "10px",
+                        background: "blue",
+                        color: "white",
+                        padding: "10px",
+                        borderRadius: "8px",
+                        zIndex: 10000,
+                        width: "150px"
+                    }}
+                >
+                    <h4>Drone Info</h4>
+                    <p>Ammo: {selectedDrone.weapons_ready}</p>
+                    <p>Status: {selectedDrone.status}</p>
+                    <button
+                        style={{
+                            width: "100%",
+                            background: "red",
+                            color: "white",
+                            border: "none",
+                            padding: "5px",
+                            marginTop: "5px"
+                        }}
+                        onClick={() => {
+                            onEngage(selectedDrone.drone_id);
+                            setSelectedDrone(null);
+                        }}
+                    >
+                        ENGAGE
+                    </button>
+                    <button
+                        style={{
+                            width: "100%",
+                            marginTop: "5px"
+                        }}
+                        onClick={() => setSelectedDrone(null)}
+                    >
+                        CLOSE
+                    </button>
+                </div>
+            )}
+        </div>
+
+    )
+}
--- client/src/icons/drone.js
+++ client/src/icons/drone.js
@@ -4,10 +4,12 @@ export const droneIcon = new L.Icon({
     iconUrl: "https://cdn-icons-png.flaticon.com/512/1830/1830867.png", // רחפן/מטוס UAV
   iconSize: [40, 40],
   iconAnchor: [20, 20],
+  className:"recon"
 });
 
 export const dronAttack = new L.Icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/512/7357/7357465.png", // דוגמה ל‑PNG רחפן צבעוני
-    iconSize: [40, 40],
-    iconAnchor: [20, 20],
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/1830/1830867.png", // רחפן/מטוס UAV
+  iconSize: [40, 40],
+  iconAnchor: [20, 20],
+  className:"attack",
 })
