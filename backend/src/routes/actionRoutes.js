6e109328fe95831b83a09d8fa0677a703f07ad8d
Daniel Levy <d0583258844@gmail.com>
2026-03-25 15:27:00 +0200

    Add all the missing files/code

--- backend/src/routes/actionRoutes.js
+++ backend/src/routes/actionRoutes.js
@@ -5,12 +5,16 @@ import {handleStrikeOrder} from "../controllers/attackController.js";
 const router = express.Router();
 
 router.get('/state', getCurrentState);
+
 // פקודת תקיפה
 router.post('/engage', handleEngage);
+
 // פקודת ניווט ידני
 router.post('/navigate', handleNavigate);
+
 // פקודת ה-Strike.  logic 
 router.post('/strike', handleStrikeOrder);
+
 // שליפת היסטוריית מסלול של רחפן
 router.get('/history/:droneId', getDroneHistory);
 
