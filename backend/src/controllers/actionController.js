a360bf5e927a9893c027359edf462fb4948820b6
Daniel Levy <d0583258844@gmail.com>
2026-03-25 16:13:33 +0200

    deleted few comantes. add cheng few lines in actionController

--- backend/src/controllers/actionController.js
+++ backend/src/controllers/actionController.js
@@ -45,7 +45,7 @@ export const handleNavigate = async (req, res) => {
         console.log(`📍 [NAVIGATION COMMAND] Received at ${new Date().toISOString()}`);
         console.log(`Drone ID: ${drone_id} to Coordinates: ${lat}, ${lon}`);
 
-        const response = await fetch('http://localhost:8001/engage', { // לפי המסמך זה אותו פורט
+        const response = await fetch('http://localhost:8001/engage', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ action, drone_id, lat, lon })
@@ -71,10 +71,9 @@ export const handleNavigate = async (req, res) => {
 export const getDroneHistory = async (req, res) => {
     try {
         const { droneId } = req.params;
-        // מושכים את הנקודות האחרונות מה-DB (האוסף שהדאטא ממלאים)
         const history = await State.find({ "recon_data.active_drone": droneId })
             .sort({ timestamp: -1 })
-            .limit(40); // 40 נקודות מספיקות לשובל יפה
+            .limit(40);
 
         const path = history.map(item => ({
             lat: item.recon_data.telemetry.lat,
@@ -88,24 +87,21 @@ export const getDroneHistory = async (req, res) => {
 };
 
 
-// server/src/controllers/actionController.js
-
 export const getCurrentState = async (req, res) => {
     try {
-        // אנחנו לוקחים את המסמך הכי עדכני שנשמר ב-DB מהדאטא
-        const latestUpdate = await State.findOne().sort({ timestamp: -1 });
+        const response = await fetch('http://localhost:8000/state', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        });
 
-        if (!latestUpdate) {
-            return res.status(404).json({ message: "No data found" });
+        if (!response.ok) {
+            throw new Error(`Data Team API returned an error ${error.message}`);
         }
 
-        // מחזירים בדיוק לפי המבנה שהחבר צריך
-        res.status(200).json({
-            timestamp: latestUpdate.timestamp,
-            target_data: latestUpdate.target_data, // וודא שב-DB זה שמור כמערך []
-            recon_data: latestUpdate.recon_data,   // וודא שב-DB זה שמור כמערך []
-            attack_data: latestUpdate.attack_data  // וודא שב-DB זה שמור כמערך []
-        });
+        const data = await response.json();
+
+        res.status(200).json(data);
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
